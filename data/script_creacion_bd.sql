967dd2d17935e8aecbaf663652b4514d5b7cd14c
jualobcab <juanjo.lc@gmail.com>
2024-12-03 18:58:51 +0100

    Commit 3/12

--- data/script_creacion_bd.sql
+++ data/script_creacion_bd.sql
@@ -5,16 +5,25 @@ CREATE TABLE usuarios (
    id INT AUTO_INCREMENT PRIMARY KEY,
    usuario VARCHAR(30) UNIQUE NOT NULL,
    password VARCHAR(100) NOT NULL,
-   rol ENUM ('admin','usuario','invitado') DEFAULT 'usuario',
+   rol ENUM('admin','usuario','invitado') DEFAULT 'usuario',
    ultimo_acceso TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 );
 CREATE TABLE productos (
    id_producto INT AUTO_INCREMENT PRIMARY KEY,
    nombre VARCHAR(30) NOT NULL,
    descripcion VARCHAR(100),
-   precio float NOT NULL
+   precio float NOT NULL,
+   magico BOOLEAN,
+   fechaAnadido DATE DEFAULT(CURRENT_DATE),
+   img BLOB NOT NULL,
+   categoria /* Meter categoria */
 );
+CREATE TABLE categorias (
+   id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(10) NOT NULL,
+   descripcion VARCHAR(100),
+)
 
 
-CREATE USER IF NOT EXISTS usuario_tienda@'localhost' IDENTIFIED BY '1234';
-GRANT SELECT, INSERT, UPDATE, DELETE on tienda.* TO usuario_tienda@'localhost';
+CREATE USER IF NOT EXISTS gamemaster@'localhost' IDENTIFIED BY '1234';
+GRANT SELECT, INSERT, UPDATE, DELETE on tienda.* TO gamemaster@'localhost';
